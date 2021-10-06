ddaf7bd2668c78f3d4dc274b56d6a9644f179623
cukmekerb <cukmekerb@gmail.com>
2021-10-06 08:36:40 -0700

    full game system

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,6 +1,8 @@
 var pubnub;
-
-async function pubnub_join_event(game_pin, username) {
+var username = "ThiccGuy69"; // TODO: username picker and stuff
+var game_key;
+var answer_channel;
+async function pubnub_join_event(game_pin) {
 	let channel_name = game_pin + username + "letmein";
 	await pubnub.subscribe({
 		channels: [channel_name]
@@ -13,7 +15,7 @@ async function pubnub_join_event(game_pin, username) {
 				channels: [channel_name]
 			});
 			pubnub.removeListener({
-				message: on_message
+				signal: on_message
 			});
 		}
 		
@@ -23,7 +25,7 @@ async function pubnub_join_event(game_pin, username) {
 	})
 }
 
-async function join_game(game_pin, username) {
+async function join_game(game_pin) {
 	pubnub = new PubNub({
 		publishKey: "pub-c-cd07b990-56dd-4e2a-9f38-5ed2bf3d968c",
 		subscribeKey: "sub-c-d12759ae-2136-11ec-8d5d-a65b09ab59bc"
@@ -34,8 +36,54 @@ async function join_game(game_pin, username) {
 	});
 	let allowed_in = await pubnub_join_event(game_pin, username);
 	if (allowed_in == "0") {
-		alert("please kindly go kill yourself");
+		console.log("please kindly go kill yourself");
 		return;
 	}
-	alert("game join accepted!!!");
+	game_key = allowed_in;
+	console.log("game join accepted!!!");
+	
+	pubnub.subscribe({
+		channels: [game_pin + "events", game_pin + username + "answer_right"]
+	});
+	answer_channel = game_pin + username + "answers";
+	
+	pubnub.addListener({
+		signal: handle_message_event
+	});
+}
+
+function handle_message_event(m) {
+	if (m.channel.endsWith("events")) {
+		// TODO: handle game events
+		if (m.message == "question") {
+			// TODO: handle question
+			console.log("question asked");
+		}
+	}
+	if (m.channel.endsWith(username + "answer_right")) {
+		// TODO: show answer right / wrong screen
+		// 0 = no answer, 1 = wrong, 2 = correct
+		switch (m.message) {
+			case 1:
+				console.log("you are a dumbass");
+				break;
+			case 2:
+				console.log("you got it right");
+				break;
+			default:
+				console.log("why're you so slow?")
+				break;
+		}
+	}
+}
+
+function answer_question(index) {
+	pubnub.publish({
+		channel: answer_channel,
+		message: {
+			username,
+			answer: index,
+			key: game_key
+		}
+	});
 }
--- src/scripts/host.js
+++ src/scripts/host.js
@@ -1,40 +1,60 @@
 var pubnub;
 var game_pin = "testgame";
-var joined_users = {};
-var join_listener = {
-	signal: (m) => {
+var users = {};
+var current_question = -1;
+var game;
+var listener = {
+	signal: m => {
 		console.log("message!")
 		console.log(m)
-		switch(m.channel) {
-			case game_pin + "joins":
-				if (!joined_users[m.message]) {
-					pubnub.signal({
-						message: true,
-						channel: game_pin + m.message + "letmein"
-					});
-					joined_users[m.message] = {
-						score: 0,
-						correct: 0
-					}
+		if (m.channel == game_pin + "joins") {
+			if (!users[m.message]) {
+				let key = Math.floor(Math.random() * 10000000);
+				pubnub.signal({
+					message: key,
+					channel: game_pin + m.message + "letmein"
+				});
+				
+				users[m.message] = {
+					score: 0,
+					correct: 0,
+					correct: [],
+					key
 				}
-				else {
-					console.log(joined_users[m.message])
-					pubnub.signal({
-						message: "0",
-						channel: game_pin + m.message + "letmein"
-					});
+				pubnub.subscribe({
+					channels: [game_pin + m.message + "answers"]
+				});
+			}
+			else {
+				console.log(users[m.message])
+				pubnub.signal({
+					message: "0",
+					channel: game_pin + m.message + "letmein"
+				});
+			}
+			return;
+		}
+	},
+	message: m => {
+		if (m.channel.endsWith("answers")) {
+			console.log("Got answer!")
+			console.log(m.channel)
+			// TODO: rng and user scoring
+			if (m.message.key == users[m.message.username].key) {
+				console.log("key is valid");
+				console.log(m.message.answer);
+				if (users[m.message.username].correct[current_question] == null) {
+					users[m.message.username].correct[current_question] = game[current_question].correct.includes(m.message.answer);
 				}
-				break
-			default:
-				console.log("peepeepoopoo");
-				console.log("message")
+			}
 		}
 	}
 };
 
-function start_game() {
+async function start_game() {
 	// TODO: game URL
-//	game_pin = Math.floor(Math.random() * 10000000);
+	game = await fetch("test_game.json").then(a => a.json());
+	game_pin = Math.floor(Math.random() * 10000000);
 	pubnub = new PubNub({
 		publishKey: "pub-c-cd07b990-56dd-4e2a-9f38-5ed2bf3d968c",
 		subscribeKey: "sub-c-d12759ae-2136-11ec-8d5d-a65b09ab59bc"
@@ -42,5 +62,35 @@ function start_game() {
 	pubnub.subscribe({
 		channels: [game_pin + "joins"]
 	});
-	pubnub.addListener(join_listener);
+	pubnub.addListener(listener);
+	document.write(game_pin);
+}
+
+function end_question() {
+	for (let i in users) {
+		if (users[i].correct[current_question] != null) {
+			pubnub.signal({
+				channel: game_pin + i + "answer_right",
+				message: users[i].correct[current_question] ? 2 : 1
+			});
+		}
+		else {
+			pubnub.signal({
+				channel: game_pin + i + "answer_right",
+				message: "0"
+			});
+			
+			users[i].correct[current_question] = false; // now they can't answer late
+		}
+	}
+	// TODO: show scoreboard
+}
+
+function next_question() {
+	current_question++;
+	// TODO: show question screen and wait
+	pubnub.signal({
+		channel: game_pin + "events",
+		message: "question"
+	});
 }
